5e928d5dbfc42e5e8684e8ca217fac18d5357cd0
Nicole Sontillano <nsontillano@freelancer.com>
2023-07-24 08:43:53 +0800

    add og image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['../demo-styling.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private metaTagService: Meta) {}
+
   title = 'angular-quickstart';
+
+  ngOnInit() {
+    this.metaTagService.addTags([
+      { property: 'og:image', content: 'https://user-images.githubusercontent.com/43764894/223549637-2715c89d-a44e-42e0-8f6c-fa6246279658.png'},
+     ]);
+  }
 }
