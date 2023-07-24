b6a781a4917a779d2549824cfb5e4ca40b27ea10
Nicole Sontillano <nsontillano@freelancer.com>
2023-07-24 08:59:56 +0800

    add og type

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.metaTagService.addTags([
+      {property: 'og:type', content: 'website'},
       { property: 'og:title', content: 'Angular Quickstart'},
       { property: 'og:description', content: 'Angular Quickstart Test'},
       { property: 'og:image', content: 'https://user-images.githubusercontent.com/43764894/223549637-2715c89d-a44e-42e0-8f6c-fa6246279658.png'},
